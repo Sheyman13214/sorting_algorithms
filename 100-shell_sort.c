86b6ec0200d16ea0ed241959520bb86c8a5ea4ab
Sheyman13214 <alexisjonesflecther@gmail.com>
2024-02-16 01:58:57 -0800

    sorting_algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,50 +1,46 @@
 #include "sort.h"
 
 /**
- * swap_int - Function that swaps two integer in an array.
- * @m: First int to swap.
- * @n: Second int to swap.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-
-void swap_int(int *m, int *n)
+void swap_ints(int *a, int *b)
 {
-	int temp;
+	int tmp;
 
-	temp = *m;
-	*m = *n;
-	*n = temp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- * shell_sort - Function that sorts an array of ints in ascending order
- * using the shell sort alogrithm
- * @array: Array of ints to sort.
- * @size: size of array.
- * Description: Uses the Knuth Interval Sequence.
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
  */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, a, b;
+	size_t gap, i, j;
 
-	/* Check if array is empty and size is less han 2 */
 	if (array == NULL || size < 2)
 		return;
 
-	/* Calculate initial gap */
 	for (gap = 1; gap < (size / 3);)
 		gap = gap * 3 + 1;
 
-	/* Perform Shell Sort */
 	for (; gap >= 1; gap /= 3)
 	{
-		for (a = gap; a < size; a++)
+		for (i = gap; i < size; i++)
 		{
-			a = b;
-			while (b >= gap && array[b - gap] > array[b])
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				swap_int(array + b, array + (b - gap));
-				b -= gap;
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
 			}
 		}
 		print_array(array, size);
